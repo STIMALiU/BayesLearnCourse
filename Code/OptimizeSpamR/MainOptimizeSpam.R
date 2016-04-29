78ae2fd864851130d655e1297ca6ef63635710dc
Matias Quiroz <quiroz.matias@gmail.com>
2016-04-29 20:43:26 +0200

    Fixed MainOptimizeSpam.R to be more numerically stable for the probit model

--- Code/OptimizeSpamR/MainOptimizeSpam.R
+++ Code/OptimizeSpamR/MainOptimizeSpam.R
@@ -5,7 +5,7 @@
 ###################################################################################
 
 ###########   BEGIN USER INPUTS   ################
-Probit <- 0 # If Probit <-0, then logistic model is used.
+Probit <- 1 # If Probit <-0, then logistic model is used.
 chooseCov <- c(1:16) # Here we choose which covariates to include in the model
 tau <- 10000; # Prior scaling factor such that Prior Covariance = (tau^2)*I
 ###########     END USER INPUT    ################
@@ -45,20 +45,29 @@ LogPostProbit <- function(betaVect,y,X,mu,Sigma){
   nPara <- length(betaVect);
   linPred <- X%*%betaVect;
                                       
-  logLik <- sum(y*log(pnorm(linPred)) + (1-y)*log(1-pnorm(linPred)) );
+  # MQ change:
+  # instead of logLik <- sum(y*log(pnorm(linPred)) + (1-y)*log(1-pnorm(linPred)) ) type in the equivalent and
+  # much more numerically stable; 
+  logLik <- sum(y*pnorm(linPred, log.p = TRUE) + (1-y)*pnorm(linPred, log.p = TRUE, lower.tail = FALSE))
+  # The old expression: logLik2 <- sum(y*log(pnorm(linPred)) + (1-y)*log(1-pnorm(linPred)) )
   abs(logLik) == Inf
+  #print('-----------------')
+  #print(logLik)
+  #print(logLik2)
   #if (abs(logLik) == Inf) logLik = -20000; # Likelihood is not finite, stear the optimizer away from here!
   logPrior <- dmvnorm(betaVect, matrix(0,nPara,1), Sigma, log=TRUE);
   return(logLik + logPrior)
 }
 
-
-
 # Calling the optimization routine Optim. Note the auxilliary arguments that are passed to the function logPost
 # Note how I pass all other arguments of the function logPost (i.e. all arguments except betaVect which is the one that we are trying to optimize over) to the R optimizer.
 # The argument control is a list of options to the optimizer. Here I am telling the optimizer to multiply the objective function (i.e. logPost) by -1. This is because
 # Optim finds a minimum, and I want to find a maximum. By reversing the sign of logPost I can use Optim for my maximization problem.
-initVal <- as.vector(solve(crossprod(X,X))%*%t(X)%*%y); # Initial values by OLS
+
+# Different starting values. Ideally, any random starting value gives you the same optimum (i.e. optimum is unique)
+initVal <- as.vector(rep(0,dim(X)[2])); 
+# Or a random starting vector: as.vector(rnorm(dim(X)[2]))
+# Set as OLS estimate: as.vector(solve(crossprod(X,X))%*%t(X)%*%y); # Initial values by OLS
 
 if (Probit==1){
   logPost = LogPostProbit;
